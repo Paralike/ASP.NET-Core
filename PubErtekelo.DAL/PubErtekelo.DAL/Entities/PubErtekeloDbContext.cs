4c44bb10ec8d47eac5c82ae061ec79e7b0ca76be
Paralike <38865450+Paralike@users.noreply.github.com>
2020-04-28 14:12:22 +0200

    change name 2

---
+++ PubErtekelo.DAL/PubErtekelo.DAL/Entities/PubErtekeloDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using PubErtekelo.DAL.Entities;
+using   Microsoft.EntityFrameworkCore;
+
+namespace PubErtekelo.DAL
+{
+    class PubErtekeloDbContext : DbContext
+    {
+        public PubErtekeloDbContext([NotNull] DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet <Felhasznalo> Felhasznalok { get; set; }
+    }
+}
