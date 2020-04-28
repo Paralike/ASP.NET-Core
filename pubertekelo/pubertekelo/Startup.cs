2a1426c8b774a31dbc0ab827d24bdd1705857ce0
Paralike <38865450+Paralike@users.noreply.github.com>
2020-04-28 15:33:40 +0200

    update startup

--- pubertekelo/pubertekelo/Startup.cs
+++ pubertekelo/pubertekelo/Startup.cs
@@ -1,14 +1,11 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using PubErtekelo.DAL;
 
 namespace pubertekelo
 {
@@ -25,6 +22,16 @@ namespace pubertekelo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<PubErtekeloDbContext>(options => 
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            }); 
+            services.AddSwaggerGen(c => 
+            {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Tanulmányi rendszer API", Version = "v1"});
+            });
+            services.AddScoped<IFelhasznaloService, DefaultFelhasznaloService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
