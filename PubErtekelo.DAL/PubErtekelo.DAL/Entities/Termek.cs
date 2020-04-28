a2ea157ffda8dfcaa94d84729a52ea9320247dfd
Paralike <38865450+Paralike@users.noreply.github.com>
2020-04-28 17:44:59 +0200

    +termekek, velemenyek

---
+++ PubErtekelo.DAL/PubErtekelo.DAL/Entities/Termek.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace PubErtekelo.DAL.Entities
+{
+    public class Termek : EntityBase
+    {
+        public int ar { get; set; }
+        public bool is_akcios { get; set; }
+        public List<string> allergenek { get; set; }
+    }
+}
---
+++ PubErtekelo.DAL/PubErtekelo.DAL/Entities/Velemeny.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PubErtekelo.DAL.Entities
+{
+    public class Velemeny : EntityBase
+    {
+        public int ertekeles;
+        public string szoveg;
+        public bool is_moderalva;
+    }
+}
