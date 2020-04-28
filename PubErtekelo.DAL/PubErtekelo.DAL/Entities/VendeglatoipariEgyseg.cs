d5f0082403beea0bef29fb572f416b34ecb420fe
Botond Ungvárszki <ungbotond@gmail.com>
2020-04-28 17:41:57 +0200

    Plusz entitások by Boti

---
+++ PubErtekelo.DAL/PubErtekelo.DAL/Entities/VendeglatoipariEgyseg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PubErtekelo.DAL.Entities
+{
+    public class VendeglatoipariEgyseg : EntityBase
+    {
+        public float Ertekeles { get; set; }
+        public string Cim { get; set; }
+        public string Telefonszam { get; set; }
+        public bool Akadalymentesitett { get; set; }
+        public bool AllatBarat { get; set; }
+        public bool CsakNagykoruak { get; set; }
+        public int Arkategoria { get; set; }
+    }
+}
