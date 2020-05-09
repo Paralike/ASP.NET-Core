16ec7a5318f27a846318788c5770c212023d5bcb
Paralike <38865450+Paralike@users.noreply.github.com>
2020-05-09 18:39:38 +0200

    add component
    26

--- Frontend/pub-ertekelo-front/src/app/app-routing.module.ts
+++ Frontend/pub-ertekelo-front/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './core/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path:'',component: }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
